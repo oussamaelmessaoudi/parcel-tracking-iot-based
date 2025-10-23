8a9fd3a2decfcecdd3c95f737436e81a0433d81b
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-10-23 18:59:45 +0100

    refactor(Sketch ino) : Add DHT + GPS Sketch script

--- electronic-side/Sketch files/DHT11/DHT11.ino
+++ electronic-side/Sketch files/DHT11/DHT11.ino
@@ -2,12 +2,16 @@
 #include <ESP8266HTTPClient.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
-DHT dht(D1, DHT11);
+// TinyGPSPlus gps;
+// SoftwareSerial gpsSerial(D7,D8);
+DHT dht(D3, DHT11);
 
-const char* ssid = "1WIFI";
-const char* password = "1234432111";
-const char* mqttServer = "broker.hivemq.com";
+const char* ssid = "TP-Link_E70F";
+const char* password = "32455847";
+const char* mqttServer = "192.168.100.253";
 const int mqttPort = 1883;
 const char* topic = "sensor/dht11";
 
@@ -16,6 +20,7 @@ PubSubClient client(espClient);
 
 void setup() {
   Serial.begin(115200);
+  // gpsSerial.begin(9600);
   WiFi.begin(ssid, password);
   dht.begin();
 
@@ -35,6 +40,9 @@ void loop() {
     client.connect("ESP8266Client");
   }
 
+  // while(gpsSerial.available() > 0){
+  //   gps.encode(gpsSerial.read());
+  // }
   float temp = dht.readTemperature();
   float humd = dht.readHumidity();
 
@@ -42,12 +50,16 @@ void loop() {
     String payload = "{";
     payload += "\"temperature\":" + String(temp) + ",";
     payload += "\"humidity\":" + String(humd);
+    // payload += "\"latitude\":" + String(gps.location.lat(),6) + ",";
+    // // payload += "\"longitude\":" + String(gps.location.lng(),6);
     payload += "}";
 
     client.publish(topic,payload.c_str());
     Serial.println("Published : "+payload);
+  }else{
+    Serial.println("Waiting for valid sensor or GPS data ...");
   }
 
   client.loop();
-  delay(10000); // Send every 10 seconds
+  delay(2000); // Send every 10 seconds
 }
