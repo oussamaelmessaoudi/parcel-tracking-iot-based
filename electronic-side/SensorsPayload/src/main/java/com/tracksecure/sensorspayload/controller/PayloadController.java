448c4c9063bfcab280ceb9415f461adbf99e24f2
nordinamb <nordinamb123@gmail.com>
2025-10-24 23:25:10 +0100

    Improve Controller

--- electronic-side/SensorsPayload/src/main/java/com/tracksecure/sensorspayload/controller/PayloadController.java
+++ electronic-side/SensorsPayload/src/main/java/com/tracksecure/sensorspayload/controller/PayloadController.java
@@ -1,4 +1,15 @@
 package com.tracksecure.sensorspayload.controller;
 
 public class PayloadController {
+<<<<<<< Updated upstream
+=======
+    private final PayloadService payloadService;
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/data")
+    // @GetMapping("/sensor/data")
+    public Payload getData(){
+        return payloadService.getPayload();
+    }
+    
+>>>>>>> Stashed changes
 }
