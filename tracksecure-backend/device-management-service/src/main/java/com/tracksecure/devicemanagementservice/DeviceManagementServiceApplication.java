055c382f383f940bac76ecbbc1f4740bb67b0a7f
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-11-19 20:26:19 +0100

    feat : init

---
+++ tracksecure-backend/device-management-service/src/main/java/com/tracksecure/devicemanagementservice/DeviceManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tracksecure.devicemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagementServiceApplication.class, args);
+    }
+
+}
---
+++ tracksecure-backend/device-management-service/src/test/java/com/tracksecure/devicemanagementservice/DeviceManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracksecure.devicemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
